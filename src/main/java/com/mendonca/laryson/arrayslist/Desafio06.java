2c15ae819c6cbfc483cdcbdc05576b3081499aed
Laryson Martins <larysonmartins008@gmail.com>
2025-08-06 21:57:35 -0300

    Atualização: Desafios ArraysList

---
+++ src/main/java/com/mendonca/laryson/arrayslist/Desafio06.java
@@ -0,0 +1,12 @@
+package com.mendonca.laryson.arrayslist;
+
+/* Desafio 06: Remoção condicional de elementos
+ * Descrição: Preencha um ArrayList com números inteiros e remova todos os elementos que sejam múltiplos de 3.
+ * Nível: Intermediário
+ */
+
+public class Desafio06 {
+    public static void main(String[] args) {
+
+    }
+}
